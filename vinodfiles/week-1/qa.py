7730a221aa5243c3c10acd084f78719bb414db60
vinod-polinati <vinodd.polinati@gmail.com>
2025-03-13 11:41:38 +0530

    removed-unnnecessary-files

--- vinodfiles/week-1/qa.py
+++
@@ -1,7 +0,0 @@
-import ollama
-
-def ask_llm(query):
-    response = ollama.chat(model="mistral", messages=[{"role": "user", "content": query}])
-    return response['message']['content']  # Extracts only the assistant's response
-
-print(ask_llm("What is Deep Learning?"))
--- vinodfiles/week-1/testingllm.py
+++
@@ -1,7 +0,0 @@
-import ollama
-
-response = ollama.chat(
-    model="mistral", 
-    messages=[{"role": "user", "content": "What is Machine Learning?"}]
-    )
-print(response['message']['content'])  
